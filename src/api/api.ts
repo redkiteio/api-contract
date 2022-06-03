e04e0563578eda1c3f8d288b08b2fb754b4a9f77
gora <gora@redkite.io>
2022-06-03 16:11:43 +0400

    fixed api

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import { store } from "../store";
 import { toggleLoading, setIsError } from "../store/mainSlice";
 
 export const defaultApi = axios.create({
-    baseURL: "http://tools.redkite.io/api/v1/",
+    baseURL: "https://tools.redkite.io/api/v1/",
 });
 
 defaultApi.interceptors.request.use((req) => {
