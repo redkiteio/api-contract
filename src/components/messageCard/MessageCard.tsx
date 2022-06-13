ae230c8091353ab0ec392888b3f3ad98b5363ff8
gora <gora@redkite.io>
2022-06-13 13:25:16 +0400

    fixes 3

--- src/components/messageCard/MessageCard.tsx
+++ src/components/messageCard/MessageCard.tsx
@@ -13,7 +13,7 @@ interface MessageCardProps {
 const MessageCard: FC<MessageCardProps> = ({ handleClose, message, messageDetails, success }) => {
     const { protocol, host } = window.location;
     // const FULL_URL = `${protocol}//${host}/api-contract/view/${messageDetails}`;
-    const FULL_URL = `${protocol}//${host}${process.env.PUBLIC_URL}/view/${messageDetails}`;
+    const FULL_URL = `${protocol}//${host}${process.env.PUBLIC_URL}/#/view/${messageDetails}`;
     console.log("FULL_URL", FULL_URL);
 
     return (
